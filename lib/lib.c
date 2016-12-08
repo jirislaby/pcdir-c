28f01bf136ee162d32b1a72218b0aaf30e92274d
Jiri Slaby <jirislaby@gmail.com>
2016-12-08 08:26:09 +0100

    lib: add some more printing functions
    Esp. for unsigned and hex.
    
    Signed-off-by: Jiri Slaby <jirislaby@gmail.com>

--- lib/lib.c
+++ lib/lib.c
@@ -13,10 +13,19 @@ GEN_PRINT2(char, "%d");
 GEN_PRINT2(short, "%d");
 GEN_PRINT2(int, "%d");
 GEN_PRINT2(long, "%ld");
+GEN_PRINT3(longlong, long long, "%lld");
 GEN_PRINT2(float, "%f");
 GEN_PRINT2(double, "%lf");
+GEN_PRINT2(size_t, "%zu");
 
-GEN_PRINT3(longlong, long long, "%lld");
+GEN_PRINT3(uchar, unsigned char, "%u");
+GEN_PRINT3(ushort, unsigned short, "%u");
+GEN_PRINT3(uint, unsigned int, "%u");
+GEN_PRINT3(ulong, unsigned long, "%lu");
+GEN_PRINT3(ulonglong, unsigned long long, "%llu");
+
+GEN_PRINT3(hex, unsigned long long, "%llx");
+
 GEN_PRINT3(character, char, "%c");
 GEN_PRINT3(string, const char *, "%s");
 GEN_PRINT3(pointer, const void *, "%p");
--- lib/lib.h
+++ lib/lib.h
@@ -9,6 +9,16 @@ void print_longlong(long long n);
 void print_float(float n);
 void print_double(double n);
 
+void print_size_t(size_t n);
+
+void print_uchar(unsigned char n);
+void print_ushort(unsigned short n);
+void print_uint(unsigned int n);
+void print_ulong(unsigned long n);
+void print_ulonglong(unsigned long long n);
+
+void print_hex(unsigned long long n);
+
 void print_character(char n);
 void print_string(const char *n);
 void print_pointer(const void *n);
